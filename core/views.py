63bbb4062f15473f88725e4f89790d34d3b8f503
Vanka7a156 <velkov_i08@abv.bg>
2025-08-05 22:06:20 +0300

    asyncio

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
+from asgiref.sync import sync_to_async
 from django.shortcuts import render
+import asyncio
 
 def landing_page(request):
     return render(request, 'core/landing.html')
 
-def about_page(request):
-    return render(request, 'core/about.html')
+async def about_page(request):
+        await asyncio.sleep(2)
+        return await sync_to_async(render)(request, 'core/about.html')
